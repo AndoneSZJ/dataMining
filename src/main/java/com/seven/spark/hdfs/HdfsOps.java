a48ca80e46c97e8da5a5a521d690d8ec4f9a9bba
seven <zjshi01@mail.nfsq.com.cn>
2018-07-06 11:20:50 +0800

    修改工程结构

--- src/main/java/com/seven/spark/hdfs/HdfsOps.java
+++ src/main/java/com/seven/spark/hdfs/HdfsOps.java
@@ -12,84 +12,69 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 public class HdfsOps {
 
+	private final static String hadoopPath = "hdfs://vm-xaj-bigdata-da-d01:8020";
 
-	public static void saveHdfs(JavaRDD<String> rdd,JavaSparkContext sc,String savePath) {
-		Configuration hadoopConf = sc.hadoopConfiguration();
-		Path hdfsPath = new Path(savePath);
-		try {
-			URI uri = new java.net.URI("hdfs://vm-xaj-bigdata-da-d01:8020");
-			FileSystem hdfs = org.apache.hadoop.fs.FileSystem.get(uri, hadoopConf);
-			if (hdfs.exists(hdfsPath)) {
-				hdfs.delete(hdfsPath, true);
-			}
-			rdd.repartition(1).saveAsTextFile(savePath);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public static void saveHdfs(JavaRDD<String> rdd, JavaSparkContext sc, String savePath) {
+		saveData(rdd, sc, savePath, 0);
 	}
 
-	public static void saveHdfs(JavaRDD<String> rdd,JavaSparkContext sc,String savePath,int part) {
-		Configuration hadoopConf = sc.hadoopConfiguration();
-		Path hdfsPath = new Path(savePath);
-		try {
-			URI uri = new java.net.URI("hdfs://vm-xaj-bigdata-da-d01:8020");
-			FileSystem hdfs = org.apache.hadoop.fs.FileSystem.get(uri, hadoopConf);
-			if (hdfs.exists(hdfsPath)) {
-				hdfs.delete(hdfsPath, true);
-			}
-			if(part==0) {
-				rdd.saveAsTextFile(savePath);
-			}else {
-				rdd.repartition(part).saveAsTextFile(savePath);
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public static void saveHdfs(JavaRDD<String> rdd, JavaSparkContext sc, String savePath, int part) {
+		saveData(rdd, sc, savePath, part);
 	}
 
-	public static void saveHdfs(JavaPairRDD<String,String> rdd,JavaSparkContext sc,String savePath) {
-		Configuration hadoopConf = sc.hadoopConfiguration();
-		Path hdfsPath = new Path(savePath);
-		try {
-			URI uri = new java.net.URI("hdfs://vm-xaj-bigdata-da-d01:8020");
-			FileSystem hdfs = org.apache.hadoop.fs.FileSystem.get(uri, hadoopConf);
-			if (hdfs.exists(hdfsPath)) {
-				hdfs.delete(hdfsPath, true);
-			}
-			rdd.repartition(1).saveAsTextFile(savePath);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+
+	private static void saveData(JavaRDD<String> rdd, JavaSparkContext sc, String savePath, int part) {
+		saveData(sc, savePath, rdd, null, part);
 	}
 
-	public static void saveHdfs(JavaPairRDD<String,String> rdd,JavaSparkContext sc,String savePath,int part) {
+	public static void saveHdfs(JavaPairRDD<String, String> rdd, JavaSparkContext sc, String savePath) {
+		saveData(rdd, sc, savePath, 0);
+	}
+
+	public static void saveHdfs(JavaPairRDD<String, String> rdd, JavaSparkContext sc, String savePath, int part) {
+		saveData(rdd, sc, savePath, part);
+	}
+
+	private static void saveData(JavaPairRDD<String, String> rdd, JavaSparkContext sc, String savePath, int part) {
+		saveData(sc, savePath, null, rdd, part);
+	}
+
+	private static void saveData(JavaSparkContext sc, String savePath,
+								 JavaRDD<String> javaRDD, JavaPairRDD<String, String> javaPairRDD, int part) {
 		Configuration hadoopConf = sc.hadoopConfiguration();
-		Path hdfsPath = new Path(savePath);
+		Path path = new Path(savePath);
 		try {
-			URI uri = new java.net.URI("hdfs://vm-xaj-bigdata-da-d01:8020");
-			FileSystem hdfs = org.apache.hadoop.fs.FileSystem.get(uri, hadoopConf);
-			if (hdfs.exists(hdfsPath)) {
-				hdfs.delete(hdfsPath, true);
+			URI uri = new URI(hadoopPath);
+			FileSystem fileSystem = FileSystem.get(uri, hadoopConf);
+			if (fileSystem.exists(path)) {
+				fileSystem.delete(path, true);
 			}
-			if(part==0) {
-				rdd.saveAsTextFile(savePath);
-			}else {
-				rdd.repartition(part).saveAsTextFile(savePath);
+			if (javaRDD != null) {
+				if (part == 0) {
+					javaRDD.repartition(1).saveAsTextFile(savePath);
+				} else {
+					javaRDD.repartition(part).saveAsTextFile(savePath);
+				}
+			} else {
+				if (part == 0) {
+					javaPairRDD.repartition(1).saveAsTextFile(savePath);
+				} else {
+					javaPairRDD.repartition(part).saveAsTextFile(savePath);
+				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static void delHdfs(JavaSparkContext sc,String savePath) {
+	public static void delHdfs(JavaSparkContext sc, String savePath) {
 		Configuration hadoopConf = sc.hadoopConfiguration();
-		Path hdfsPath = new Path(savePath);
+		Path path = new Path(savePath);
 		try {
-			URI uri = new java.net.URI("hdfs://vm-xaj-bigdata-da-d01:8020");
-			FileSystem hdfs = org.apache.hadoop.fs.FileSystem.get(uri, hadoopConf);
-			if (hdfs.exists(hdfsPath)) {
-				hdfs.delete(hdfsPath, true);
+			URI uri = new URI(hadoopPath);
+			FileSystem fileSystem = FileSystem.get(uri, hadoopConf);
+			if (fileSystem.exists(path)) {
+				fileSystem.delete(path, true);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
